bf517c01f0bce9e17fc03bd7411f5fbd2c9dfed8
mmgonnar <gn.mariela@gmail.com>
2025-09-24 19:15:54 -0600

    feat: :building_construction: Updated administrative team images to use external URLs
    Didn´t work

--- features/team/utils/constants.ts
+++ features/team/utils/constants.ts
@@ -1,8 +1,19 @@
 export const administrativeTeam =[
-    {id:1, name:"Dr. Arturo Jaimes", position:"Director", imageUrl:"../img/director.png"},
-    {id:2, name:"M.C. Rocio Peralta", position:"Administración Escolar", imageUrl:"../img/sub_academica.png"},
-    {id:3, name:"M.H. Hugo Martínez", position:"Funciones Sustantivas", imageUrl:"../img/sub_func_susnt.png"},
-    {id:4, name:"Lic. Guadalupe Millán", position:"Planeación y Evaluación", imageUrl:"../img/planeacion.png.png"},
-    {id:5, name:"M.C. Tania Zacaría", position:"Viculación", imageUrl:"../img/vinculacion.png"},
-    {id:6, name:"M.C. Estela Gutierrez", position:"Sistema Abierto", imageUrl:"../img/sit_abierto.png"},
+    {id:1, name:"Dr. Arturo Jaimes", position:"Director", imageUrl:"https://randomuser.me/api/portraits/men/76.jpg"},
+    {id:2, name:"M.C. Rocio Peralta", position:"Administración Escolar", imageUrl:"https://randomuser.me/api/portraits/women/76.jpg"},
+    {id:3, name:"M.H. Hugo Martínez", position:"Funciones Sustantivas", imageUrl:"https://randomuser.me/api/portraits/men/76.jpg"},
+    {id:4, name:"Lic. Guadalupe Millán", position:"Planeación y Evaluación", imageUrl:"https://randomuser.me/api/portraits/women/76.jpg"},
+    {id:5, name:"M.C. Tania Zacaría", position:"Viculación", imageUrl:"https://randomuser.me/api/portraits/women/76.jpg"},
+    {id:6, name:"M.C. Estela Gutierrez", position:"Sistema Abierto", imageUrl:"https://randomuser.me/api/portraits/women/76.jpg"},
+    // {id:7, name:"Psic. Rosa Ma. Olivares", position:"Director", imageUrl:"https://randomuser.me/api/portraits/men/76.jpg"},
+    // {id:8, name:"Dr. Arturo Jaimes", position:"Director", imageUrl:"https://randomuser.me/api/portraits/men/76.jpg"},
 ]
+
+// export const administrativeTeam =[
+//     {id:1, name:"Dr. Arturo Jaimes", position:"Director", imageUrl:"../img/director.png"},
+//     {id:2, name:"M.C. Rocio Peralta", position:"Administración Escolar", imageUrl:"../img/sub_academica.png"},
+//     {id:3, name:"M.H. Hugo Martínez", position:"Funciones Sustantivas", imageUrl:"../img/sub_func_susnt.png"},
+//     {id:4, name:"Lic. Guadalupe Millán", position:"Planeación y Evaluación", imageUrl:"../img/planeacion.png.png"},
+//     {id:5, name:"M.C. Tania Zacaría", position:"Viculación", imageUrl:"../img/vinculacion.png"},
+//     {id:6, name:"M.C. Estela Gutierrez", position:"Sistema Abierto", imageUrl:"../img/sit_abierto.png"},
+// ]
