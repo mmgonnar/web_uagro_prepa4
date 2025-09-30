caafdf87afb14dfb3bb18dd7ada35f34db909951
mmgonnar <gn.mariela@gmail.com>
2025-09-30 09:25:03 -0600

    feat: :construction:

--- features/announcements/components/announcements.tsx
+++ features/announcements/components/announcements.tsx
@@ -9,12 +9,15 @@ export default function Announcements() {
   const date = new Date();
   return (
     <div>
-      <Card animation="scale" className="border-l-main-red border-2 border-l-3">
+      <Card
+        animation="scale"
+        className="border-l-main-red max-w-78 border-2 border-l-3"
+      >
         <div className="flex items-center gap-3">
           <Label labelText="Inscripciones" variant="announcement" />
           <p className="text-xs text-neutral-400">25 sept 2025</p>
         </div>
-        <p className="w-100 text-left text-base text-neutral-600">
+        <p className="w-10 text-left text-base text-neutral-600">
           Ya están abiertas las inscripciones para el nuevo ciclo escolar.
           Consulta los requisitos y fechas importantes.
         </p>
