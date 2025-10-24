744b549d094c9805a586ea80c286d06b4ba9591c
mmgonnar <gn.mariela@gmail.com>
2025-10-23 18:41:54 -0600

    feat: :art: modified teams carousel

--- features/ui/components/cards.tsx
+++ features/ui/components/cards.tsx
@@ -4,10 +4,10 @@ import { cn } from '@/utils/functions';
 import Label from './label';
 import { ReactNode } from 'react';
 import { administrativeTeam } from '@/features/team/utils/constants';
+import Image from 'next/image';
 
 const animations = {
-  scale:
-    'transition-all duration-300 hover:z-10 hover:scale-[1.02] sm:hover:scale-[0]',
+  scale: 'transition-all duration-300  hover:scale-[1.02] sm:hover:scale-[1]',
   none: '',
 };
 interface CardProps {
@@ -36,18 +36,21 @@ export default function Card({
 export function TeamCards() {
   return (
     <>
-      {administrativeTeam.map((item, index) => (
-        <Card key={index} className="h-95 pb-8">
-          <div className="flex flex-col items-center justify-between pb-8">
-            <img
-              className="w-70 rounded-t-md"
-              src="https://randomuser.me/api/portraits/men/76.jpg"
-              alt="Director Dr. Arturo Jaimes Vega"
-            />
-            <div className="m-auto flex w-70 flex-col items-center justify-center gap-2">
-              <h3 className="w-full">{item.name}</h3>
-              <Label variant="announcement" labelText={item.position} />
-            </div>
+      {administrativeTeam.map(item => (
+        <Card
+          key={item.id}
+          className="pb-8m-auto m-auto flex h-95 flex-col items-center justify-between pb-8"
+        >
+          <Image
+            src={item.src}
+            alt={`Foto de ${item.name}`}
+            width={100}
+            height={100}
+            className="w-70 rounded-t-md object-cover"
+          />
+          <div className="m-auto flex w-70 flex-col items-center justify-center gap-2">
+            <h3 className="w-full">{item.name}</h3>
+            <Label variant="announcement" labelText={item.position} />
           </div>
         </Card>
       ))}
